a184b49a1b5b03018416b34bceb7fc394969077a
kylvia <hh.todd@qq.com>
2016-03-18 18:01:26 +0800

    3.18

--- easyuiPractice/js/index.js
+++ easyuiPractice/js/index.js
@@ -164,7 +164,7 @@ $("#box").resizable({
 	
 	
 	//***********CP8:
-	$('#box').panel({
+	/*$('#box').panel({
 		width:400,
 		height: 150,
 		title: '面板',
@@ -178,8 +178,283 @@ $("#box").resizable({
 		style:{
 			'min-height': '100px'
 		}
+	});*/
+	
+	//***********CP14:
+	/*$("#box").layout({
+		//fit: true,
+	});
+	//$("#box").layout('collapse','north');
+	$("#box").layout('add',{
+		title: 'south',
+		region: 'south',
+		height:100
+	});
+	$(document).click(function(){
+		$("#box").layout().css('display','block');
+		$("#box").layout('resize');
+	});
+	console.log($("#box").layout("options"));*/
+	
+	//***********CP15:
+	/*$('#box').window({
+		title : '窗口',
+		width :500,
+		height : 250,
+//		collapsible : false,
+//		minimizable : false,
+//		maximizable : false,
+//		closable : false,
+//		closed : true,
+//		zIndex : 9999,
+//		draggable : false,
+//		resizable : false,
+//		shadow : true,
+//		modal : true,//在窗口后面加幕布
+		inline : true,
+//		fit : true,
+		iconCls : 'icon-edit',
+		
 	});
 	
+	$(document).click(function(){
+		$("#box").window('move',{
+			left : 0,
+			top : 0
+		});
+	});
+	$(document).dblclick(function(){
+		$("#box").window('vcenter');
+	});*/
+	
+	//***********CP16:
+	/*$("#box").dialog({
+		title : "JQ对话框",
+		content: 'welcome！',
+//		collapsible : true,
+//		minimizable : true,
+//		maximizable : true,
+//		resizable : true,
+		toolbar : [{
+			text : '编辑',
+			iconCls : 'icon-edit',
+			plain: false, //扁平化
+			handler : function(){
+				alert('编辑');
+			}
+		},{
+			text : '保存',
+			iconCls : 'icon-save',
+			handler : function(){
+				alert('保存');
+			}
+		}],
+		buttons : [{
+			text : '编辑',
+			iconCls : 'icon-edit',
+			plain: true,
+			handler : function(){
+				alert('编辑');
+			}
+		},{
+			text : '取消',
+			iconCls : 'icon-cancel',
+			handler : function(){
+				alert('取消');
+			}
+		}]
+	});
+	console.log($('#box').dialog('dialog'));*/
+	
+	//***********CP17:
+/*	$.messager.defaults = {
+		ok : '是',
+		cancel : '取消',
+		width:400,
+		height:300
+	}
+	
+//	显示警告窗口。参数：
+//	$.messager.alert('警告框','这是一个信息框','info',function(){
+//		alert('确认后的回调！');
+//	});
+//	title：在头部面板显示的标题文本。
+//	msg：显示的消息文本。
+//	icon：显示的图标图像。可用值有：error,question,info,warning。
+//	fn: 在窗口关闭的时候触发该回调函数。
+	
+	
+	//使用确认框，三个参数均可选
+//	$.messager.confirm('确认对话框','你真的要删除吗?',function(flag){
+//		if(flag){
+//			alert("删除成功！");
+//		}
+//	});
+//	显示一个包含“确定”和“取消”按钮的确认消息窗口。参数：
+//	title：在头部面板显示的标题文本。
+//	msg：显示的消息文本。
+//	fn(b): 当用户点击“确定”按钮的时侯将传递一个 true 值给回调函数，否则传递一个false 值。
+
+	//使用提示框，三个参数均可选
+//	$.messager.prompt('提示信息','请输入你的名字：',function(flag){
+//		if(flag){
+//			alert(flag);
+//		}
+//	});
+//	显示一个用户可以输入文本的并且带“确定”和“取消”按钮的消息窗体。参数：
+//	title：在头部面板显示的标题文本。
+//	msg：显示的消息文本。
+//	n(val): 在用户输入一个值参数的时候执行的回调函数。
+
+	//进度条信息
+	$.messager.progress({
+		title : '执行中',
+		msg : '努力加载中...',
+		text : '{value}%',
+		interval : 100,
+		modal: true
+	});
+//	显示一个进度消息窗体。
+//	属性定义为：
+//	title：在头部面板显示的标题文本。默认：空。
+//	msg：显示的消息文本。默认：空。
+//	text：在进度条上显示的文本。默认：undefined。
+//	interval：每次进度更新的间隔时间。默认：1000 毫秒。
+
+	console.log($.messager.progress('bar'));
+	setTimeout(function(){
+		$.messager.progress('close');
+	},3000);
+	
+	$.messager.show({
+		title : '我的消息',
+		msg : '消息在5秒后关闭',
+		showSpeed : 1000,
+		showType : 'slide',
+		style : {
+			top : 30,
+			left : 50
+		},
+		timeout : 5000,
+	});*/
+	
+	//***********CP18:
+	/*$(document).on('contextmenu',function(e){
+		e.preventDefault();
+		$('#box').menu('show',{
+			top : e.pageY,
+			left : e.pageX,
+		});
+		//添加菜单项
+//		$('#box').menu('appendItem',{
+//			id : 'new',
+//			parent : $('#box').menu('findItem','打开').target,
+//			text : '添加',
+//			iconCls : 'icon-add',
+//			onclick : function(){
+//				alert('新增');
+//			}
+//		});
+		
+		//设置某个菜单项图标
+		$('#box').menu('setIcon',{
+			target : '#box-save',
+			iconCls : 'icon-save'
+		});
+		$('#box').menu('removeItem','#box-save');
+		//$('#box').menu('disableItem','#box-save');
+	});
+	//console.log($('#box').menu('findItem','保存').id);*/
+	
+	//***********CP19:
+	/*$('#edit').menubutton({
+		plain : true,
+		menu : "#menu",
+		duration : 500,
+		iconCls : 'icon-edit',
+	});
+	console.log($('#edit').menubutton('options'));
+//	$('#edit').menubutton('disable');
+//	$('#edit').menubutton('destroy');
+	$.fn.menubutton.defaults.cursor = "pointer";*/
+	
+	
+	//***********CP20:
+	/*$('#edit').splitbutton({
+		menu : '#menu',
+		iconCls : 'icon-edit',
+		plain : true,
+	});*/
+	
+	
+	//***********CP21:
+	/*$('#box').pagination({
+		total : 2000,
+		pageSize : 10,
+		pageNumber : 3
+	});*/
+	
+	
+	//***********CP22:
+	/*$('#ss').searchbox({
+		searcher: function(value,name){
+			alert(value+','+name);
+		},
+		menu : '#box',
+		prompt: '请输入内容',
+		width: 200,
+		height : 30,
+		disabled : false,
+	});
+	console.log($('#ss').searchbox('options'));
+	console.log($('#ss').searchbox('textbox'));
+	console.log($('#ss').searchbox('getValue'));
+	console.log($('#ss').searchbox('getName'));
+	
+//	$('#ss').searchbox('destroy');
+//	$('#ss').searchbox('clear');
+	$('#ss').searchbox('resize',500);
+	$(document).dblclick(function(){
+		$('#ss').searchbox('reset');
+	});
+	
+	var m = $('#ss').searchbox('menu');
+	var item = m.menu('findItem','体育');
+	m.menu('setIcon',{
+		target : item.target,
+		iconCls : 'icon-save'
+	});*/
+	
+	
+	//***********CP23:
+//	$('#email').validatebox({
+//		required : true,
+//		validType : 'email',
+//		delay : 2000,
+//		missingMessage : '请按正确的方式填写',
+//		invalidMessage : '无效时',
+//		tipPosition : 'left',
+//		deltaX : 120,
+//		novalidate : true,
+//	});
+//	
+//	console.log($('#email').validatebox('validate'));
+//	console.log($('#email').validatebox('isValid'));
+	
+	$.extend($.fn.validatebox.defaults.rules,{
+		minLength: {
+			validator : function(value,param){
+				return value.length >= param[0];
+			},
+			message : '请输入长度不小于{0}的字符',
+		}
+	});
+	
+	//调用
+	$("#email").validatebox({
+		required : true,
+		validType : 'minLength[5]',
+	});
 });
 
 /*$.parser.auto = false;
